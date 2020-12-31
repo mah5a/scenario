39ee253357a39dd7fa12558292f99c4ed3c45c12
mah5a <mahsa.karamimehr@gmail.com>
2020-12-31 10:33:39 +0330

    iris changed

--- iris.py
+++ iris.py
@@ -1,9 +1,20 @@
 import codecs
 import csv
 import urllib
+from extend_list import ExtendList
 
 
 def main():
     url = "https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv"
     stream = urllib.request.urlopen(url)
     csv_file = csv.reader(codecs.iterdecode(stream, 'utf-8'))
+    print(type(csv_file))
+
+    iris_lst = []
+    for row in csv_file:
+        IRIS = ExtendList(row)
+        iris_lst.append(IRIS)
+
+
+if __name__ == '__main__':
+    main()
